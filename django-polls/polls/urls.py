c49abf08810643a3f1a4858ba64ecf7386feca6b
scottschmidl <sjschmidl@gmail.com>
2022-07-26 08:28:20 -0500

    move polls from mysite to django-polls as the tutorial says

--- django-polls/polls/urls.py
+++ django-polls/polls/urls.py
@@ -4,19 +4,6 @@ from . import views
 
 app_name = 'polls'
 
-# urlpatterns = [path(route='',
-#                     view=views.index, name='index'),
-
-#                # path('specifics/<int:question_id>/', views.detail, name='detail'),
-#                path(route='<int:question_id>/',
-#                     view=views.detail, name='detail'),
-
-#                path(route='<int:question_id>/results/',
-#                     view=views.results, name='results'),
-
-#                path(route='<int:question_id>/vote/',
-#                     view=views.vote, name='vote'),]
-
 urlpatterns = [path('', views.IndexView.as_view(), name='index'),
                path('<int:pk>/', views.DetailView.as_view(), name='detail'),
                path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
